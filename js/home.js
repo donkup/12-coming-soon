8e765fbaa80186ee9d414ac39cd71f750cb991c3
Donatas <donkuprys@gmail.com>
2021-05-20 20:46:41 +0300

    Clock as Class

--- js/home.js
+++ js/home.js
@@ -1,8 +1,9 @@
 // IMPORT
 import { Clock } from "./components/clock/Clock.js";
 import { socials } from "./components/socials/socials.js";
-import {socialsData} from "./data/socialsData.js"
+import { socialsData } from "./data/socialsData.js";
 
 // EXECUTION
-Clock('#clock_1');
-socials('footer .socials', socialsData );
+new Clock('#clock_1');
+// new Clock(485545);
+socials('footer .socials', socialsData);

7eec77702ec69dfa07ef7b2197f7e0c9a1fc4aac
Donatas <donkuprys@gmail.com>
2021-06-30 18:03:52 +0300

    toast

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 
 // IMPORT
+import { Toast } from "./components/toast/Toast.js";
 import { Clock } from "./components/clock/Clock.js";
 import { Form } from "./components/form/Form.js";
 import { Progressbar } from "./components/progress-bar/ProgressBar.js";
@@ -10,20 +11,52 @@ import { socialsData } from "./data/socialsData.js";
 import { SocialsOOP } from "./components/socials/SocialsOOP.js";
 
 // EXECUTION
+const toast = new Toast();
+// toast.success('Tau pavyko!');
+// toast.error('kazkur sufeilinai pildydamas💩')
+
 new Clock('#clock_1', clockData);
+
+
 // socials('footer .socials', socialsData);
 new Progressbar('.left-column', progressBarData);
 new SocialsOOP('.row-big.socials', socialsData);
 new Form('.hero form');
 new Form('main form');
 
-let count = 0;
 
-const timer1 = setInterval(() => {
-    if (count < 10) {
-        console.log(count++);
-    } else {
-        clearInterval(timer1);
-    }
-}, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let count = 0;
+
+// const timer1 = setInterval(() => {
+//     if (count < 10) {
+//         console.log(count++);
+//     } else {
+//         clearInterval(timer1);
+//     }
+// }, 1000)
 
